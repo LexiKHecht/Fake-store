db5f0bab0c9af46dd9c5de5a1b2ef041b0a0345a
Lexi <lexikhecht@gmail.com>
2024-01-22 21:17:55 -0500

    profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,22 +15,10 @@ router.get('/', async (req, res) => {
 
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-    });
+router.get('/profile', async (req, res) => {
 
-    const user = userData.get({ plain: true });
 
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  res.render('profile');
 });
 
 router.get('/login', (req, res) => {
