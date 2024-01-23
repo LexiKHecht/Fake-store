9119743c8624bac86c0297bbdcd3e42e7438a780
jessicanwey <jessica.n.wey@gmail.com>
2024-01-22 21:55:22 -0500

    Added call to API just for all products

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const storeRoutes = require('./storeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/', storeRoutes);
 
 module.exports = router;
