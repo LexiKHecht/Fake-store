9216ded801d322c30fe51c75afa4f9fc3ae5785f
jessicanwey <jessica.n.wey@gmail.com>
2024-01-29 19:57:10 -0500

    added back working ratings

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,11 +3,11 @@ module.exports = {
     const roundedRating = Math.round(rating);
 
     if (roundedRating === 1) {
-      return `<input id='one-star' type="checkbox" name="rating-3" class="mask mask-heart bg-red-400 cursor-pointer" disabled checked/>
-      <input id='two-star' type="checkbox" name="rating-3" class="mask mask-heart bg-orange-400 cursor-pointer" disabled/>
-      <input id='three-star' type="checkbox" name="rating-3" class="mask mask-heart bg-yellow-400 cursor-pointer" disabled/>
-      <input id='four-star' type="checkbox" name="rating-3" class="mask mask-heart bg-lime-400 cursor-pointer" disabled/>
-      <input id='five-star' type="checkbox" name="rating-3" class="mask mask-heart bg-green-400 cursor-pointer" disabled/>`;
+      return `<input id='one-star' type="checkbox" name="rating-3" class="mask mask-heart bg-red-400 cursor-default" disabled checked/>
+      <input id='two-star' type="checkbox" name="rating-3" class="mask mask-heart bg-orange-400 cursor-default" disabled/>
+      <input id='three-star' type="checkbox" name="rating-3" class="mask mask-heart bg-yellow-400 cursor-default" disabled/>
+      <input id='four-star' type="checkbox" name="rating-3" class="mask mask-heart bg-lime-400 cursor-default" disabled/>
+      <input id='five-star' type="checkbox" name="rating-3" class="mask mask-heart bg-green-400 cursor-default" disabled/>`;
     } else if (roundedRating === 2) {
       return `<input id='one-star' type="checkbox" name="rating-3" class="mask mask-heart bg-red-400 cursor-default" disabled/>
       <input id='two-star' type="checkbox" name="rating-3" class="mask mask-heart bg-orange-400 cursor-default" disabled checked/>
